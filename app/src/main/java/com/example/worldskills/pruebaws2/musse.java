374c34dc3a52b39ff691f9bff2cc5343ff0d7173
Brahiam Musse <bamusse@misena.edu.co>
2018-08-30 10:39:31 -0500

    2

--- app/src/main/java/com/example/worldskills/pruebaws2/musse.java
+++ app/src/main/java/com/example/worldskills/pruebaws2/musse.java
@@ -9,5 +9,7 @@ public class musse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_musse);
+
+        //jhyvgh
     }
 }
